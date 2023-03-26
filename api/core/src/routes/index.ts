7413ae305c2e71d4f7ae8490c244c6c430f9bb08
Rodrigo Polydoro <rspolydoro@gmail.com>
2023-03-26 16:01:55 -0300

    Modified cors

--- api/core/src/routes/index.ts
+++ api/core/src/routes/index.ts
@@ -4,7 +4,9 @@ import {challengerRoute} from "./challenger.route";
 import {questionRoute} from "./question.routes";
 
 const routes = Router();
+const cors = require('cors');
 
+routes.use(cors())
 routes.use("/session", sessionRoute);
 routes.use("/challenger", challengerRoute);
 routes.use("/question", questionRoute);
--- api/core/src/server.ts
+++ api/core/src/server.ts
@@ -7,13 +7,7 @@ const bodyParser = require('body-parser')
 const port = process.env.PORT || 3000;
 const cors = require('cors');
 
-const corsOptions = {
-  origin : ["https://great-volleyball-gold-ireland.bohr.io", "http://localhost:5173"],
-  allowedHeaders : "Content-Type",
-  methods: ["GET","POST"]
-};
-app.use(cors(corsOptions));
-app.options('*', cors())
+app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use('/',routes)
