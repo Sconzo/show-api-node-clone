8495a7efdd1f15a1cc3dcd91eb142c3e0328dc01
Rodrigo Polydoro <rspolydoro@gmail.com>
2023-04-04 20:43:06 -0300

    adding cors

--- api/core/src/server.ts
+++ api/core/src/server.ts
@@ -1,13 +1,28 @@
 import express from 'express';
+
 import {routes} from "./routes";
 
 
 const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser')
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:8080'];
+type CorsCallback = (error: Error | null, success: boolean) => void;
+app.use(cors({
+  origin: function (origin : string, callback: CorsCallback) {
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'),false);
+    }
+  }
+}));
+
 app.use('/',routes)
 
 app.get('/' ,function(req:any, res:any){
